649f07302a0ff8f331d4a59a65c7255070f4c682
Dpe <dep@dep.com>
2020-06-25 22:48:44 -0300

    Adicionando o cliente junto a ordem de servico recem criada

--- osworks-api/src/main/java/com/algaworks/osworks/domain/service/GestaoOrdemServicoService.java
+++ osworks-api/src/main/java/com/algaworks/osworks/domain/service/GestaoOrdemServicoService.java
@@ -3,10 +3,14 @@ package com.algaworks.osworks.domain.service;
 import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.OrderComparator.OrderSourceProvider;
 import org.springframework.stereotype.Service;
 
+import com.algaworks.osworks.domain.exception.NegocioException;
+import com.algaworks.osworks.domain.model.Cliente;
 import com.algaworks.osworks.domain.model.OrdemServico;
 import com.algaworks.osworks.domain.model.StatusOrdemServico;
+import com.algaworks.osworks.domain.repository.ClienteRepository;
 import com.algaworks.osworks.domain.repository.OrdemServicoRepository;
 
 @Service
@@ -14,9 +18,15 @@ public class GestaoOrdemServicoService {
 	
 	@Autowired
 	private OrdemServicoRepository ordemServicoRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	public OrdemServico criar(OrdemServico ordemServico) {
-		 
+		
+		Cliente cliente = clienteRepository.findById(ordemServico.getCliente().getId())
+				.orElseThrow(() -> new NegocioException("Cliente não encontrado") );
+	     
+		 ordemServico.setCliente(cliente);
 		 ordemServico.setDataAbertura(LocalDateTime.now());
 		 ordemServico.setStatus(StatusOrdemServico.ABERTA);
 		 
